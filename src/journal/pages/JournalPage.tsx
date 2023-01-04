892f2cb2c555fa1af4d51a951a7af7dcf604a21a
Víctor Merino <victor.merino@jungheinrich.es>
2023-01-04 13:26:23 +0100

    Add Button fixed to the bottom right

--- src/journal/pages/JournalPage.tsx
+++ src/journal/pages/JournalPage.tsx
@@ -1,5 +1,5 @@
-import { MailOutline } from '@mui/icons-material'
-import { Typography } from '@mui/material'
+import { AddOutlined, MailOutline } from '@mui/icons-material'
+import { IconButton, Typography } from '@mui/material'
 import React from 'react'
 
 import { JournalLayout } from '../layout/JournalLayout'
@@ -9,8 +9,22 @@ export const JournalPage = () => {
   return (
     <JournalLayout>
       <>
-        {/* <NothingSelectedView /> */}
-        <NoteView />
+        <NothingSelectedView />
+        {/* <NoteView /> */}
+
+        <IconButton
+          size="large"
+          sx={{
+            color: 'white',
+            backgroundColor: 'error.main',
+            ':hover': { backgroundColor: 'error.main', opacity: 0.9 },
+            position: 'fixed',
+            right: 50,
+            bottom: 50,
+          }}
+        >
+          <AddOutlined sx={{ fontSize: '30px' }} />
+        </IconButton>
       </>
     </JournalLayout>
   )
